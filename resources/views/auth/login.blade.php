3eef6195677d0aaf1458c76d0c047a2c7528ad00
Polisss <poli60@seznam.cz>
2022-03-03 18:24:54 +0100

    úprava formulářů login/register + přidané css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,38 +7,47 @@
 @section('content')
     <form class="box box-auth" method="POST" action="{{ route('login') }}">
         @csrf
-        <!-- Logo -->
-        <h2 class="box-auth-heading">
-            @yield('logo')
-            {{ trans('auth.login') }}
-        </h2>
+        @if ( !Auth::check() )
+            <!-- Logo -->
+            <h2 class="box-auth-heading">
+                @yield('logo')
+                {{ trans('auth.login') }}
+            </h2>
 
-        <!-- Email Address -->
-        <x-input placeholder="{{ trans('auth.email') }}" class="form-control" type="email" name="email" :value="old('email')" required autofocus />
-
-        <!-- Password -->
-        <x-input class="form-control"
-        placeholder="{{ trans('auth.password') }}"
-        type="password"
-        name="password"
-        required autocomplete="current-password" 
-        />
+            <!-- Email Address -->
+            <x-input placeholder="{{ trans('auth.email') }}" class="form-control" type="email" name="email" :value="old('email')" required autofocus />
+            
+            <!-- Password -->
+            <x-input class="form-control"
+            placeholder="{{ trans('auth.password') }}"
+            type="password"
+            name="password"
+            required autocomplete="current-password" 
+            />
+            
+            <!-- Remember Me -->
+            <label class="checkbox">
+                <input type="checkbox" name="remember"  checked >
+                <span>{{ trans('auth.remember_me') }}</span>
+            </label>
         
-        <!-- Remember Me -->
-        <label class="checkbox">
-            <input type="checkbox" name="remember"  checked >
-            <span>{{ trans('auth.remember_me') }}</span>
-        </label>
         
-        <x-button class="btn-primary">
-            {{ trans('auth.login') }}
-        </x-button>
+            <x-button class="btn-primary">
+                {{ trans('auth.login') }}
+            </x-button>
 
-        <a class="btn-primary g-home" href="{{ route('ghome') }}">{{ trans('auth.continue_guest') }}</a>
+            <a class="btn-primary g-home" href="{{ route('ghome') }}">{{ trans('auth.continue_guest') }}</a>
 
-        <p class="alt-action">
-        {{ trans('auth.or') }} <a href=" {{ route('register') }} ">{{ trans('auth.create_account') }}</a>
-        </p>
+            <p class="alt-action">
+            {{ trans('auth.or') }} <a href=" {{ route('register') }} ">{{ trans('auth.create_account') }}</a>
+            </p>
+        @else
+            <div class="home-a">
+                <!-- Logo -->
+                @yield('logo')
+                <a class="btn-primary a-home" href="{{ route('home') }}">{{ trans('auth.login') }}</a>
+            </div>
+        @endif
     </form>
 @endsection
 </x-guest-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,41 +7,48 @@
     @section('content')  
         <form class="box box-auth" method="POST" action="{{ route('register') }}">
             @csrf
+            @if ( !Auth::check() )
+                <!-- Logo -->
+                <h2 class="box-auth-heading">
+                    @yield('logo')
+                    {{ __('auth.register') }}
+                </h2>
 
-            <!-- Logo -->
-            <h2 class="box-auth-heading">
-                @yield('logo')
-                {{ __('auth.register') }}
-            </h2>
+                <!-- Name -->
+                <input type="text" name="name" placeholder="{{ __('auth.nickname') }}" class="form-control" required autofocus :value="old('name')">
 
-            <!-- Name -->
-            <input type="text" name="name" placeholder="{{ __('auth.nickname') }}" class="form-control" required autofocus :value="old('name')">
+                <!-- Email Address -->
+                <x-input placeholder="{{ __('auth.email') }}" class="form-control" type="email" name="email" :value="old('email')" required />
 
-            <!-- Email Address -->
-            <x-input placeholder="{{ __('auth.email') }}" class="form-control" type="email" name="email" :value="old('email')" required />
+                <!-- Password -->
+                <x-input class="form-control"
+                placeholder="{{ __('auth.password') }}"
+                type="password"
+                name="password"
+                required autocomplete="new-password" 
+                />
 
-            <!-- Password -->
-            <x-input class="form-control"
-            placeholder="{{ __('auth.password') }}"
-            type="password"
-            name="password"
-            required autocomplete="new-password" 
-            />
+                <!-- Confirm Password -->
+                <x-input class="form-control"
+                placeholder="{{ __('auth.password_again') }}"
+                type="password"
+                name="password_confirmation" required 
+                />
 
-            <!-- Confirm Password -->
-            <x-input class="form-control"
-            placeholder="{{ __('auth.password_again') }}"
-            type="password"
-            name="password_confirmation" required 
-            />
-
-            <x-button class="btn-primary">
-                {{ __('auth.register') }}
-            </x-button>
-            
-            <p class="alt-action">
-            {{ __('auth.or') }} <a href=" {{ route('login') }} ">{{ __('auth.sign_up') }}</a>
-            </p>
+                <x-button class="btn-primary">
+                    {{ __('auth.register') }}
+                </x-button>
+                
+                <p class="alt-action">
+                {{ __('auth.or') }} <a href=" {{ route('login') }} ">{{ __('auth.sign_up') }}</a>
+                </p>                
+            @else
+                <div class="home-a">
+                    <!-- Logo -->
+                    @yield('logo')
+                    <a class="btn-primary a-home" href="{{ route('home') }}">{{ trans('auth.login') }}</a>
+                </div>
+            @endif
         </form>
     @endsection
 </x-guest-layout>
