747baa0c69c4364069c232d9546ba34e353a50df
Polisss <poli60@seznam.cz>
2022-03-08 12:23:00 +0100

    bugfix - oprava kopírování linku svého porofilu + oprava výpisu errorů, a css

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,3 @@
-;(function($, window, document, undefined){
-/*
-* ADD FORM
-*/
-$('#add-form').find('input[name=title]').focus();
-/*
-* DELETE FORM
-*/
-$('#delete-form').on('submit', function () 
-{
-    if( $('html')[0].lang  == 'cz' ){
-        return confirm('Určitě?');
-    }else{
-        return confirm('For sure?');
-    }
-});
 /*
 * MY-POSTS
 * copy address to share
@@ -39,6 +23,22 @@ function copy() {
         });;
     });
 }
+;(function($, window, document, undefined){
+/*
+* ADD FORM
+*/
+$('#add-form').find('input[name=title]').focus();
+/*
+* DELETE FORM
+*/
+$('#delete-form').on('submit', function () 
+{
+    if( $('html')[0].lang  == 'cz' ){
+        return confirm('Určitě?');
+    }else{
+        return confirm('For sure?');
+    }
+});
 /*
 * DISCUSSION
 */
@@ -59,14 +59,14 @@ discussion.find('form').on('submit', function (event) {
             if (err.status == 422) { // when status code is 422, it's a validation issue
                 // display errors on each form field
                 $.each(err.responseJSON.errors, function (i, error) {
-                    var el = $(document).find(".btn-primary");
-                    el.after($('<span class="arrow-up err">'+error[0]+'</span>'));
+                    var el = $(document).find("textarea");
+                    el.before($('<span class="arrow-up err">'+error[0]+'</span>'));
                 });
             }
             if (err.status == 401) { //when status code is 401, there is a problem validating the rules
                 // display errors
-                var el = $(document).find(".btn-primary");
-                el.after($('<span class="arrow-up err">'+err.responseJSON.message+'</span>'));
+                var el = $(document).find("textarea");
+                el.before($('<span class="arrow-up err">'+err.responseJSON.message+'</span>'));
             }
         }
     });
